c221dbdbda56fe69085c9bb808776acaec9ae232
Masoud <60979824+MasoudHD@users.noreply.github.com>
2023-02-25 22:46:57 +0330

    20230225-1045 | Remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mheid/OneDrive/Desktop/Thermocouple
-# Build directory: C:/Users/mheid/OneDrive/Desktop/Thermocouple/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
