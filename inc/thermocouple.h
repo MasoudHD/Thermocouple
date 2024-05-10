993d9b158174c9f2f2481db8887c007c767d2b1f
MasoudHeidari <m.heidari92@gmail.com>
2024-05-10 20:51:01 +0330

    20240510-1850 | Support of Lookup Table (K-Type for now), some minor bugs fixed

--- inc/thermocouple.h
+++ inc/thermocouple.h
@@ -5,15 +5,25 @@
  extern "C" {
 #endif
 
+#define LOOKUP_TABLE
+
 #define TC_K_TYPE           0
 // #define TC_J_TYPE           1
 // #define TC_S_TYPE           2
 // #define TC_T_TYPE           3
 
+#ifdef LOOKUP_TABLE
+  #define TC_TB_K_TYPE_LEN  1643
+  #define TC_TB_K_MIN       -270
+  #define TC_TB_K_MAX       1372
+  // extern const long int TC_TB_K_TYPE[TC_TB_K_TYPE_LEN];
+#endif
+
 #define ERR_OVER_VOLTAGE    (float)0xffff
 #define ERR_LOW_VOLTAGE     (float)0xefff
 
 #define DEBUG               printf
+
 /**
  * @brief           enum of different type of TC
  * 
@@ -37,6 +47,7 @@ typedef enum
   TU_K          
 }TempUnit;
 
+
 /**
  * @brief           The structure of the ADC that is used for reading TC voltage
  * 
@@ -45,15 +56,28 @@ typedef struct
 {
     int     Resolution;
     int     AdcValue;
-    float   Vref;
-    float   Offset;
-    float   Gain; 
-    float   Voltage;
+    #ifdef LOOKUP_TABLE
+      int   Vref;
+      int   Offset;
+      int   Gain; 
+      int   Voltage;
+    #else
+      float   Vref;
+      float   Offset;
+      float   Gain; 
+      float   Voltage;
+    #endif
 }TcAdcHandler;
 
-float readTcVoltage(TcAdcHandler* hTcAdc);
-float getTcTemp(TcType type, float voltage, float cjc,TempUnit tu);
 float unitConvert(TempUnit from, TempUnit to, float temp);
+
+#ifdef LOOKUP_TABLE
+  void readTcVoltage(TcAdcHandler* hTcAdc);
+  int getTcTemp(TcType type, long int voltage, int cjc,TempUnit tu);
+#else
+  void readTcVoltage(TcAdcHandler* hTcAdc);
+  float getTcTemp(TcType type, float voltage, float cjc,TempUnit tu);
+#endif
 
 
 #ifdef __cplusplus
--- main.c
+++ main.c
@@ -5,15 +5,30 @@ int main()
 {
     TcAdcHandler hTcAdc;
 
+#ifndef LOOKUP_TABLE
     hTcAdc.Resolution = 1024;
     hTcAdc.Vref = 5;
     hTcAdc.Offset = 0;
     hTcAdc.Gain = 1;
     hTcAdc.AdcValue = 10;
     readTcVoltage(&hTcAdc);
+     //Let's assume that the voltage is 27.17mV (remove the following line in real application)
     hTcAdc.Voltage = 27.17/1000.0;
-
-    float temp = getTcTemp(TC_TYPE_K, hTcAdc.Voltage, 0, TU_C);
+    float cjcTemp = 25.3;
+    float temp = getTcTemp(TC_TYPE_K, hTcAdc.Voltage, cjcTemp, TU_C);
     printf("mV = %f, TC = %f\n", hTcAdc.Voltage*1000, temp);
+#else
+    hTcAdc.Resolution = 1024;
+    hTcAdc.Vref = 5000; //Multiplied by 1000
+    hTcAdc.Offset = 0; //Multiplied by 1000
+    hTcAdc.Gain = 1000; //Multiplied by 1000
+    hTcAdc.AdcValue = 10;
+    readTcVoltage(&hTcAdc);
+     //Let's assume that the voltage is 27.17mV (remove the following line in real application)
+    hTcAdc.Voltage = 27170; //Multiplied by 1000
+    int cjcTemp = 25300; //Multiplied by 1000
+    int temp = getTcTemp(TC_TYPE_K, hTcAdc.Voltage, cjcTemp, TU_C); //Multiplied by 1000
+    printf("mV = %d, TC = %d.%d\n", hTcAdc.Voltage, temp/1000, temp%1000);
+#endif
     return 0;
 }
